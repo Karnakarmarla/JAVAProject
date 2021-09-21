0b4f6c00b862f37a366ebb3792eaf652f0ab805a
marlakarnakar <karnakarmarla933@gmail.com>
2021-09-21 22:23:10 +0530

    java loops

---
+++ src/OneToTen.java
@@ -0,0 +1,11 @@
+public class OneToTen {
+    public static void onetoten()
+    {
+        for(int i=1;i<=10;i++)
+            System.out.println(i);
+    }
+    public static void main(String[] args)
+    {
+        onetoten();
+    }
+}
