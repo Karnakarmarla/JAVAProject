849ec6a14ab6ada3b7c0946c6de53a4ec477476e
marlakarnakar <karnakarmarla933@gmail.com>
2021-09-22 09:12:28 +0530

    java loops

---
+++ src/Day1/PowerEven.java
@@ -0,0 +1,7 @@
+public class PowerEven {
+    public void random()
+    {
+
+    }
+
+}
---
+++ src/Day1/Squares.java
@@ -0,0 +1,11 @@
+public class Squares {
+        public void squares()
+        {
+            for(int i=1;i<=10;i++)
+                System.out.println(i*i);
+        }
+        public static void main(String[] args)
+        {
+            new Squares().squares();
+        }
+}
